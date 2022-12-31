35e24a0a4bbebd48419ce5eaeeb776ec03d9800c
Mhmd <mhmd.tarek197@gmail.com>
2022-12-31 22:15:31 +0200

    ADD: Header layout

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,8 +1,10 @@
 import "./app.css";
+import Header from "../layouts/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-black dark:text-white">App</h1>
     </>
   );
