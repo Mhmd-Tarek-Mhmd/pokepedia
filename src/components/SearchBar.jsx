24bd4ae11e03f3d4068008a338531fc4f59148c9
Mhmd <mhmd.tarek197@gmail.com>
2022-12-31 23:01:54 +0200

    EDIT: SearchBar styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,11 +24,11 @@ function SearchBar({ setQuery }) {
         onChange={handleChangeVal}
         aria-label="Search by name"
         placeholder="Search by name"
-        className="flex-1 outline-none pl-3 pr-2 rounded border border-gray-400 dark:border-transparent placeholder:font-medium placeholder:text-sm"
+        className="min-w-[100px] flex-1 outline-none pl-3 pr-2 rounded border border-gray-400 dark:border-transparent placeholder:font-medium placeholder:text-sm"
       />
       <button
         type="submit"
-        className="px-6 rounded text-white font-medium bg-green-500 hover:opacity-80"
+        className="px-6 rounded text-white font-medium bg-green-500 dark:bg-green-600 hover:opacity-80"
       >
         Search
       </button>
