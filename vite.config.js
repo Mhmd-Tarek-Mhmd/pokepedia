f26321c87cee9f84558e337f789f1253c7b32a3e
Mhmd <biboo.biboo97@gmail.com>
2025-08-09 06:03:04 +0300

    FIX: routing config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
+      routesDirectory: './src/App/routes',
     }),
     tailwindcss(),
     react(),
