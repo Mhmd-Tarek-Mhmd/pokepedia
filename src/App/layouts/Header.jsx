0750bb9b5a9eec305f01770f8ed191a10b0fd348
Mhmd <biboo.biboo97@gmail.com>
2025-08-09 06:18:20 +0300

    CHANGE logo to a home link

--- src/App/layouts/Header.jsx
+++ src/App/layouts/Header.jsx
@@ -6,12 +6,13 @@ export default function Header() {
   return (
     <header className="h-20 grid place-items-center bg-white dark:bg-gray-800 shadow-sm sticky top-0 z-10">
       <div className="container flex items-center justify-between">
-        <div
+        <Link
+          to="/"
           className="flex items-center space-x-2 text-2xl font-bold"
         >
           <img aria-hidden="true" className="w-8 h-8" src="/favicon.ico" alt="" />
           <h1>PokéPedia</h1>
-        </div>
+        </Link>
 
         <div className="flex gap-2">
           <Navigations />
